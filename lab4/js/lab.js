74b12988bfc1512ef7e4ed6276e4351da7dda74d
ichigoo33 <ichco233@gmail.com>
2022-10-17 19:57:02 -0700

    lab 5 update

---
+++ lab4/js/lab.js
@@ -0,0 +1,13 @@
+// Summary. (use period)
+// Description. (use period)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author AuthorName.
+// @since  x.x.x
+
+//Ourline for creating a minesweeper game:
+//Set the number of mines, columns, and rows
+//Create the board
+//Accept player input
+//Show the result of the input
+//Check if the new input fulfills winning or losing condition
---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+ * Auhtor: Wang Liao
+ *Created: Oct 17
+ *License: Public Domain
+ */
+
+// Define Variables
+make = "Tesla";
+model = "Model 3";
+color = "White";
+year = 2019;
+
+// Calculate
+age = 2022 - year;
+
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
