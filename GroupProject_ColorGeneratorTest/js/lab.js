e39759385122a9d2d571f99bb859e6b5520419c7
ichigoo33 <ichco233@gmail.com>
2022-10-31 10:32:44 -0700

    update random color generator

--- GroupProject_ColorGeneratorTest/js/lab.js
+++ GroupProject_ColorGeneratorTest/js/lab.js
@@ -16,29 +16,30 @@ var colorResult;
 outputButton.onclick = function()
 {
   /*
-   *console.log("--------------------Phase 1--------------------");
-   *console.log("colors: ", colorsEl);
-   *console.log("childNodes: ", colorsEl.childNodes);
-   *console.log("length: ", colorsEl.childNodes.length);
-   *console.log("The number of colors = ", (colorsEl.childNodes.length-1)/2);
+  console.log("--------------------Phase 1--------------------");
+  console.log("colors: ", colorsEl);
+  console.log("childNodes: ", colorsEl.childNodes);
+  console.log("length: ", colorsEl.childNodes.length);
+  console.log("The number of colors = ", (colorsEl.childNodes.length-1)/2);
 
-   *console.log("--------------------Phase 2--------------------");
+  console.log("--------------------Phase 2--------------------");
 
-   *console.log("First child is: ", colorsEl.childNodes[1], ". Let's change its background color!");
-   *colorsEl.childNodes[1].style.backgroundColor = "blue";
+  console.log("First child is: ", colorsEl.childNodes[1], ". Let's change its background color!");
+  colorsEl.childNodes[1].style.backgroundColor = "blue";
 
-   *console.log("Now let's try to change the color with number!");
-   *colorsEl.childNodes[1].style.backgroundColor = "#ff0000ff";
+  console.log("Now let's try to change the color with number!");
+  colorsEl.childNodes[1].style.backgroundColor = "rgba(255, 34, 48, 0.8)";
 
   console.log("--------------------Phase 3--------------------");
 
   console.log("Finally, let's try change the color using a function!");
-  getRandomColor();
+  getRandomColor(0);
+
   */
 
   console.log("--------------------Phase 4--------------------");
 
-  console.log("Final test for all colors!")
+  //console.log("Final test for all colors!")
 
   for(let i = 0; i < colors_Num; i++)
   {
@@ -48,26 +49,20 @@ outputButton.onclick = function()
 
 function getRandomColor(x)
 {
-  colorResult = "#";
-  for(let i = 0; i < randomColor.length; i++)
+  colorResult = "rgba(";
+  for(let i = 0; i < 4; i++)
   {
-    if(i != randomColor.length - 1)
+    if(i < 3)
     {
-      randomColor[i] = Math.floor(Math.random() * 100);
+      colorResult += Math.floor(Math.random() * 256).toString();
+      colorResult += ", "
     }
     else
     {
-      randomColor[i] = Math.floor(Math.random() * 80 + 20);
+      colorResult += (Math.random() * 0.9 + 0.1).toString();
+      colorResult += ")"
     }
-
-    if(randomColor[i] < 10)
-    {
-      colorResult += "0";
-    }
-    colorResult += randomColor[i].toString();
   }
-  //console.log("randomColor: ", randomColor);
-
   console.log(colorResult);
   colorsEl.childNodes[2*x+1].style.backgroundColor = colorResult;
   return;
